2ce07f7f64e9f0e31f92f0cd22662f274529dc36
Wisdom Jere <wisdomjere5@gmail.com>
2022-01-21 10:37:13 +0200

    Add data annotations

--- SampleApp/Models/Incident.cs
+++ SampleApp/Models/Incident.cs
@@ -6,10 +6,15 @@ public class Incident
 	public int Id {get; set;}
 	public int InvestigatingOfficerId {get; set; }
         public DateTime IncidentDate {get; set; } 
+	[Required]
         public string IncidentCategory { get; set; }
+	[Required]
 	public string Status {get; set;}
+	[Required]
 	public string IncidentLocation {get; set;}
+	[Required]
 	public string InvestigationFindings {get; set;}
+	[Required]
 	public string Resolution {get; set;}
 	public DateTime DateClosed {get; set;}
 	public InvestigatingOfficer InvestigatingOfficer { get; set; }
--- SampleApp/Models/InvestigatingOfficer.cs
+++ SampleApp/Models/InvestigatingOfficer.cs
@@ -4,7 +4,10 @@ public class InvestigatingOfficer
 {
 	[Key]
 	public int Id {get; set;}
+	[Required]
 	public string FirstName {get; set;}
+	[Required]
 	public string LastName {get; set;}
+	[Required]
 	public string Department {get; set;}
 }
